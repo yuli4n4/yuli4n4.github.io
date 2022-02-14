2aaee1fd4199349e4d07953350123ae53beb264a
adipatiarya <aryaadipati2@gmail.com>
2022-02-15 01:22:06 +0700

    aa

---
+++ precache-manifest.89e125323649147953306a33f98ca603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3873c796e485e79fbc34ff79a140f51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0f165267d106f4fcc4",
+    "url": "/static/js/2.f88be2dd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f88be2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2949dcdef97eddad503f",
+    "url": "/static/js/main.56436148.chunk.js"
+  },
+  {
+    "revision": "f17ea0e7d7f6feff620c",
+    "url": "/static/js/runtime-main.b39e9b45.js"
+  }
+]);
