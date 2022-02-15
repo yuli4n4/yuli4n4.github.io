8d61c2d609903bb123ac4d06f822e9cd26249f08
adipatiarya <aryaadipati2@gmail.com>
2022-02-15 14:59:28 +0700

    asa

--- precache-manifest.f857af1acd066fb53142ab77d1f06c95.js
+++ precache-manifest.f857af1acd066fb53142ab77d1f06c95.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5cd92dca87425dd933c3610e9a59b357",
+    "revision": "a788840e122ea15a4591c823e0351487",
     "url": "/index.html"
   },
   {
-    "revision": "0b0f165267d106f4fcc4",
-    "url": "/static/js/2.f88be2dd.chunk.js"
+    "revision": "099105f1f8e7e7a1a3d4",
+    "url": "/static/js/2.f965215c.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.f88be2dd.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f965215c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c93e1694f4b58fb21354",
-    "url": "/static/js/main.7108d138.chunk.js"
+    "revision": "b912da200c6f4c17c657",
+    "url": "/static/js/main.ae5a883c.chunk.js"
   },
   {
     "revision": "f17ea0e7d7f6feff620c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b7d15c568616fe5e1f988fa1e194f86.js"
+  "/precache-manifest.f857af1acd066fb53142ab77d1f06c95.js"
 );
 
 self.addEventListener('message', (event) => {
